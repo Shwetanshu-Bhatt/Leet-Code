feb428218755055bfe9a36d70c011f43037b153c
Shwetanshu-Bhatt <shwetanshubhatt@.com>
2025-02-17 18:39:54 +0530

    DSA with C

---
+++ LeetCode1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize - 1; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                *returnSize = 2;
+                int* result = (int*)malloc(2 * sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
+
+int main() {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int returnSize;
+    int* result = twoSum(nums, 4, target, &returnSize);
+
+    if (result != NULL) {
+        printf("Indices: [%d, %d]\n", result[0], result[1]);
+        free(result);
+    } else {
+        printf("No solution found.\n");
+    }
+    return 0;
+}
